06ff33a2ae6f37cac45e808cf769618ef4d2afde
muhemmad-1303 <muhammedb@mozilor.com>
2023-10-11 14:38:08 +0530

    newcookie

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('cookieyes_consent_update',function(eventData){
-    if(eventData.details.include('accepted')){
+    if(eventData.details.hasOwnPropert('accepted')){
         console.log("i am accepted");
     }
     
