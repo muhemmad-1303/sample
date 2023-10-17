f2827cca82461d3f9b39c758d74210bf7610b342
muhemmad-1303 <muhammedb@mozilor.com>
2023-10-17 13:11:55 +0530

    hi

--- script.js
+++ script.js
@@ -3,12 +3,37 @@
     
 // })
 
-document.addEventListener("DOMContentLoaded", function() {
-    var queryString = location.search.substring(1);
-    var revisitStatus= queryString.split("=")[1];
-    if(revisitStatus==="yes"){
-        revisitCkyConsent();
-        console.log("hello")
-    }
+// document.addEventListener("DOMContentLoaded", function() {
+//     var queryString = location.search.substring(1);
+//     var revisitStatus= queryString.split("=")[1];
+//     if(revisitStatus==="yes"){
+//         revisitCkyConsent();
+
+//         console.log("hello")
+//     }
  
-})
+// })
+
+
+var checktimeout = 0;
+window.addEventListener("load", function () {
+  waitForElement(".cky-consent-container", function () {
+    var queryString = location.search.substring(1);
+        var revisitStatus= queryString.split("=")[1];
+       if(revisitStatus==="yes"){
+            revisitCkyConsent();
+    
+         console.log("hello")
+       }
+  });
+});
+function waitForElement(selector, callback) {
+  const element = document.querySelector(selector);
+  if (element) return callback();
+  checktimeout++;
+  if (checktimeout < 120) {
+    setTimeout(function () {
+      waitForElement(selector, callback);
+    }, 500);
+  }
+}
