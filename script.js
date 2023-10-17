9dc66034ca16d520e4bff5341acfacb08cd2a3ee
muhemmad-1303 <muhammedb@mozilor.com>
2023-10-17 13:01:03 +0530

    added

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-document.addEventListener('cookieyes_consent_update',function(eventData){
- console.log(eventData);
+// document.addEventListener('cookieyes_consent_update',function(eventData){
+//  console.log(eventData);
     
+// })
+
+document.addEventListener("DOMContentLoaded", function() {
+    var queryString = location.search.substring(1);
+    var revisitStatus= queryString.split("=")[1];
+    if(revisitStatus==="yes"){
+        revisitCkyConsent();
+        console.log("hello")
+    }
+ 
 })
