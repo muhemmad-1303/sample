70868907ac46a9891ad94288bedcf536ba5df7ea
muhemmad-1303 <muhammedb@mozilor.com>
2023-10-17 13:20:00 +0530

    news

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-// document.addEventListener('cookieyes_consent_update',function(eventData){
-//  console.log(eventData);
-    
-// })
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     var queryString = location.search.substring(1);
-//     var revisitStatus= queryString.split("=")[1];
-//     if(revisitStatus==="yes"){
-//         revisitCkyConsent();
-
-//         console.log("hello")
-//     }
- 
-// })
-
 
 var checktimeout = 0;
 window.addEventListener("load", function () {
   waitForElement(".cky-consent-container", function () {
     var queryString = location.search.substring(1);
-        var revisitStatus= queryString.split("=")[1];
-       if(revisitStatus==="yes"){
+        var renewStatus= queryString.split("=")[1];
+       if(renewConsent==="true"){
             revisitCkyConsent();
-    
-         console.log("hello")
        }
   });
 });
