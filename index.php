4ef3a2c0c931391eaf77a9ad7ad27782f639e672
muhemmad-1303 <muhammedb@mozilor.com>
2023-11-06 15:50:59 +0530

    initial v3

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Google Tag Manager -->
+
+  <!-- Start cookieyes banner --> <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/4c1e09ff98f2f7fbd9cfc4af/script.js"></script> <!-- End cookieyes banner -->
+  <!-- End cookieyes banner -->
+
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+
+  <!-- End Google Tag Manager (noscript) -->
+  <?php
+  phpinfo();
+  ?>
+  <h1>hello</h1>
+  <a href="cookie.html">cookiepolicy</a>
+  <br>
+  <a href="home.html">home</a>
+  <iframe width="560" id="frame" height="315" src="https://www.youtube.com/embed/m0IsbkfONL4?si=1szjhTIPEYkRJOCo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+
+</body>
+
+</html>
