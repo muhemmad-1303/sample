6d24c6434a108d0c784e7aadde93aee1ee05a606
muhemmad-1303 <muhammedb@mozilor.com>
2023-11-08 09:12:03 +0530

    new

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-var checktimeout = 0;
-window.addEventListener("load", function () {
-  waitForElement("cky-consent-bar", function () {
-    console.log("hello");
+
+
+    
     fetch("https://directory.cookieyes.com/api/v1/ip")
       .then((response) => response.json())
       .then((data) => console.log(data))
       .catch((error) => console.error("Error:", error));
-  });
-});
-function waitForElement(selector, callback) {
-  const element = document.querySelector(selector);
-  if (element) return callback();
-  checktimeout++;
-  if (checktimeout < 120) {
-    setTimeout(function () {
-      waitForElement(selector, callback);
-    }, 500);
-  }
-}
+ 
+
