c8e88ad9fa3fdcedbbd15bce62e7b86130ff6487
muhemmad-1303 <muhammedb@mozilor.com>
2023-11-15 15:14:03 +0530

    newwww

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-    fetch("https://directory.cookieyes.com/api/v1/ip")
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error("Error:", error));
+   
  
 
