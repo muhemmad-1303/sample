59f223e6d621367ad0424058fddc6edb69cb6e35
muhemmad-1303 <muhammedb@mozilor.com>
2023-11-21 16:27:55 +0530

    nooooo

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ window.addEventListener("load", function () {
   waitForElement(".cky-consent-container", function () {
 
     var rejectButtons = document.querySelector(".cky-notice-btn-wrapper .cky-btn.cky-btn-reject");
-    rejectButtons.replaceWith(button.cloneNode(true));
+    rejectButtons.replaceWith(rejectButtons.cloneNode(true));
     rejectButtons.onclick = function () {
       console.log("hello");
       revisitCkyConsent();
     };
+   
   });
 });
 function waitForElement(selector, callback) {
