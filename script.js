10edb46bd4a1691f3a93e959720ff43685a6e581
muhemmad-1303 <muhammedb@mozilor.com>
2023-11-21 17:24:29 +0530

    changes

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 var checktimeout = 0;
 window.addEventListener("load", function () {
   waitForElement(".cky-consent-container", function () {
-    const rejectButtons = document.getElementsByClassName(
-      "cky-btn cky-btn-reject"
-    );
-
-    for (const button of rejectButtons) {
-      button.replaceWith(button.cloneNode(true));
-    }
-    const count=0;
-    for (const button of rejectButtons) {
-      if(count===0){
-      button.onclick = function () {
-        revisitCkyConsent()
-      };
-    }
-    }
+    var rejectButtons = document.querySelector(".cky-notice-btn-wrapper .cky-btn.cky-btn-reject");
+    var newButton=rejectButtons.cloneNode(true)
+    rejectButtons.replaceWith(newButton);
+    rejectButtons.onclick = function () {
+      console.log("hello");
+      revisitCkyConsent();
+    };
   });
 });
 function waitForElement(selector, callback) {
@@ -28,6 +20,3 @@ function waitForElement(selector, callback) {
     }, 500);
   }
 }
-   
- 
-
