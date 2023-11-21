d3f01bb98018008a1735f6b95a998c17a314f053
muhemmad-1303 <muhammedb@mozilor.com>
2023-11-21 15:17:06 +0530

    new

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+var checktimeout = 0;
+window.addEventListener("load", function () {
+  waitForElement(".cky-consent-container", function () {
+    const rejectButtons = document.getElementsByClassName(
+      "cky-btn cky-btn-reject"
+    );
 
+    for (const button of rejectButtons) {
+      button.replaceWith(button.cloneNode(true));
+    }
+    for (const button of rejectButtons) {
+      button.onclick = function () {
+        console.log("hello")
+        revisitCkyConsent()
+      };
+    }
+  });
+});
+function waitForElement(selector, callback) {
+  const element = document.querySelector(selector);
+  if (element) return callback();
+  checktimeout++;
+  if (checktimeout < 120) {
+    setTimeout(function () {
+      waitForElement(selector, callback);
+    }, 500);
+  }
+}
    
  
 
