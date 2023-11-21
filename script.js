933c425aadff4e16f98ee71a1437d6cebf6b0d52
muhemmad-1303 <muhammedb@mozilor.com>
2023-11-21 17:49:29 +0530

    pr changes

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ var checktimeout = 0;
 window.addEventListener("load", function () {
   waitForElement(".cky-consent-container", function () {
     var rejectButtons = document.querySelector(".cky-notice-btn-wrapper .cky-btn.cky-btn-reject");
+    if(rejectButtons){
     var newButton=rejectButtons.cloneNode(true)
     rejectButtons.replaceWith(newButton);
-    if(rejectButtons){
     newButton.onclick = function () {
       const outerLayer=document.querySelector(".cky-consent-container");
       outerLayer.classList.add('cky-hide')
       revisitCkyConsent();
     };
   }
+  
   });
 });
 function waitForElement(selector, callback) {
