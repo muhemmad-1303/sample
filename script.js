c981abab416c8394d36acc43e3ebfb8244c10ba9
muhemmad-1303 <muhammedb@mozilor.com>
2023-11-27 12:27:48 +0530

    hello

--- script.js
+++ script.js
@@ -1,37 +1,11 @@
 var checktimeout = 0;
 window.addEventListener("load", function () {
   waitForElement(".cky-consent-container", function () {
-    const rejectButtons = document.getElementsByClassName(
-      "cky-btn cky-btn-reject"
-    );
-    if (rejectButtons) {
-      for (const button of rejectButtons) {
-        button.replaceWith(button.cloneNode(true));
-      }
-      for (const button of rejectButtons) {
-        button.onclick = function () {
-          document
-            .querySelectorAll(".cky-legitimate-switch-wrapper input")
-            .forEach((btn) => {
-              if (btn.checked === true) {
-                btn.checked = true;
-              }
-              document.querySelectorAll(".cky-switch input").forEach((btn)=>{
-                     if(btn.checked === true){
-                         btn.checked=false;
-                     }
-                })
-              document
-                .querySelectorAll(".cky-consent-switch-wrapper input")
-                .forEach((btn) => {
-                  if (btn.checked === true) {
-                    btn.checked = false;
-                  }
-                });
-            });
-          performBannerAction("accept_partial");
-        };
-      }
+    var queryString = location.search.substring(1);
+    var  action= queryString.split("=")[1];
+    if (action==="optout") {
+      document.getElementById("ckyCCPAOptOut").checked = true;
+      performBannerAction();
     }
   });
 });
@@ -46,3 +20,50 @@ function waitForElement(selector, callback) {
   }
 }
 
+// var checktimeout = 0;
+// window.addEventListener("load", function () {
+//   waitForElement(".cky-consent-container", function () {
+//     const rejectButtons = document.getElementsByClassName(
+//       "cky-btn cky-btn-reject"
+//     );
+//     if (rejectButtons) {
+//       for (const button of rejectButtons) {
+//         button.replaceWith(button.cloneNode(true));
+//       }
+//       for (const button of rejectButtons) {
+//         button.onclick = function () {
+//           document
+//             .querySelectorAll(".cky-legitimate-switch-wrapper input")
+//             .forEach((btn) => {
+//               if (btn.checked === true) {
+//                 btn.checked = true;
+//               }
+//               document.querySelectorAll(".cky-switch input").forEach((btn)=>{
+//                      if(btn.checked === true){
+//                          btn.checked=false;
+//                      }
+//                 })
+//               document
+//                 .querySelectorAll(".cky-consent-switch-wrapper input")
+//                 .forEach((btn) => {
+//                   if (btn.checked === true) {
+//                     btn.checked = false;
+//                   }
+//                 });
+//             });
+//           performBannerAction("accept_partial");
+//         };
+//       }
+//     }
+//   });
+// });
+// function waitForElement(selector, callback) {
+//   const element = document.querySelector(selector);
+//   if (element) return callback();
+//   checktimeout++;
+//   if (checktimeout < 120) {
+//     setTimeout(function () {
+//       waitForElement(selector, callback);
+//     }, 500);
+//   }
+// }
