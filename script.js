0857c0697ee576c413b5c486def05832cac4da2b
muhemmad-1303 <muhammedb@mozilor.com>
2024-02-27 11:42:52 +0530

    initial

--- script.js
+++ script.js
@@ -124,3 +124,85 @@
  
 //         console.log(scanResult.cookieData[url].scanStatus);
 // });
+
+var checktimeout = 0;
+
+window.addEventListener("DOMContentLoaded", function () {
+  waitForElement(".cky-consent-container", function () {
+    custumFunction();
+  });
+});
+
+
+function waitForElement(selector, callback) {
+  const element = document.querySelector(selector);
+  if (element) return callback();
+  checktimeout++;
+  if (checktimeout < 120) {
+    setTimeout(function () {
+      waitForElement(selector, callback);
+    }, 500);
+  }
+}
+
+document.addEventListener("cookieyes_consent_update", function (eventData) {
+   if (eventData.detail.accepted.includes("analytics")) {
+     removePlaceholder();
+    }
+    else{
+        addPlaceholder();
+    }
+});
+
+
+function custumFunction() {
+  const consent = getCkyConsent();
+  if (!consent.categories.functional && !checkPlaceholder()) {
+    addPlaceholder();
+  } else if (consent.categories.functional) {
+    removePlaceholder();
+  }
+
+
+  function addPlaceholder() {
+    const divElement = document.createElement("div");
+    divElement.setAttribute("class", "video-placeholder-youtube");
+    divElement.setAttribute("data-cky-tag", "video-placeholder");
+    divElement.setAttribute(
+      "style",
+      "height: 100%; width: 100%; background-image: url(https://cdn-cookieyes.com/assets/images/cky-placeholder.svg); background-size: 80px; background-position: center; background-repeat: no-repeat; background-color: #b2b0b059; display: flex; align-items: flex-end;"
+    );
+    const paragraph = document.createElement("p");
+    paragraph.setAttribute("class", "video-placeholder-text-youtube");
+    paragraph.setAttribute("data-cky-tag", "placeholder-title");
+    paragraph.setAttribute(
+      "style",
+      "display: block; border-color: rgb(0, 0, 0); background-color: rgb(0, 0, 0); color: rgb(255, 255, 255);"
+    );
+    paragraph.textContent = "Please accept cookies to access this content";
+    divElement.appendChild(paragraph);
+    const embedResponsiveElement = document.querySelector(
+      ".helloMuhammed"
+    );
+    if (embedResponsiveElement) {
+      embedResponsiveElement.appendChild(divElement);
+    }
+  }
+
+  function removePlaceholder() {
+    const embedResponsiveElement = document.querySelector(
+      ".helloMuhammed"
+    );
+    if (embedResponsiveElement) {
+      const divElementToRemove = embedResponsiveElement.querySelector(
+        ".video-placeholder-youtube"
+      );
+      if (divElementToRemove) {
+        embedResponsiveElement.removeChild(divElementToRemove);
+      }
+    }
+  }
+  function checkPlaceholder() {
+    return document.querySelector(".video-placeholder-youtube");
+  }
+}
