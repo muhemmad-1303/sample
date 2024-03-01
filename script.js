1bd3c0a51cd972e133f7c7578dc199a197122425
muhemmad-1303 <muhammedb@mozilor.com>
2024-03-01 07:44:29 +0530

    pr changes

--- script.js
+++ script.js
@@ -125,83 +125,87 @@
 //         console.log(scanResult.cookieData[url].scanStatus);
 // });
 
-var checktimeout = 0;
-
-window.addEventListener("DOMContentLoaded", function () {
-  waitForElement(".cky-consent-container", function () {
-    custumFunction();
-  });
-});
 
 
-function waitForElement(selector, callback) {
-  const element = document.querySelector(selector);
-  if (element) return callback();
-  checktimeout++;
-  if (checktimeout < 120) {
-    setTimeout(function () {
-      waitForElement(selector, callback);
-    }, 500);
-  }
-}
+// var checktimeout = 0;
 
-document.addEventListener("cookieyes_consent_update", function (eventData) {
-   if (eventData.detail.accepted.includes("analytics")) {
-      removePlaceholder();
-    }
-    else if (!checkPlaceholder()){
-       addPlaceholder();
-    }
-});
+// window.addEventListener("DOMContentLoaded", function () {
+//   waitForElement(".cky-consent-container", function () {
+//     custumFunction();
+//   });
+// });
 
 
-function custumFunction() {
-  const consent = getCkyConsent();
-  if (!consent.categories.functional && !checkPlaceholder()) {
-    addPlaceholder();
-  } else if (consent.categories.functional) {
-    removePlaceholder();
-  }
- }
+// function waitForElement(selector, callback) {
+//   const element = document.querySelector(selector);
+//   if (element) return callback();
+//   checktimeout++;
+//   if (checktimeout < 120) {
+//     setTimeout(function () {
+//       waitForElement(selector, callback);
+//     }, 500);
+//   }
+// }
 
-  function addPlaceholder() {
-    const divElement = document.createElement("div");
-    divElement.setAttribute("class", "video-placeholder-youtube");
-    divElement.setAttribute("data-cky-tag", "video-placeholder");
-    divElement.setAttribute(
-      "style",
-      "height: 100%; width: 100%; background-image: url(https://cdn-cookieyes.com/assets/images/cky-placeholder.svg); background-size: 80px; background-position: center; background-repeat: no-repeat; background-color: #b2b0b059; display: flex; align-items: flex-end;"
-    );
-    const paragraph = document.createElement("p");
-    paragraph.setAttribute("class", "video-placeholder-text-youtube");
-    paragraph.setAttribute("data-cky-tag", "placeholder-title");
-    paragraph.setAttribute(
-      "style",
-      "display: block; border-color: rgb(0, 0, 0); background-color: rgb(0, 0, 0); color: rgb(255, 255, 255);"
-    );
-    paragraph.textContent = "Please accept cookies to access this content";
-    divElement.appendChild(paragraph);
-    const embedResponsiveElement = document.querySelector(
-      ".helloMuhammed"
-    );
-    if (embedResponsiveElement) {
-      embedResponsiveElement.appendChild(divElement);
-    }
-  }
+// document.addEventListener("cookieyes_consent_update", function (eventData) {
+//    if (eventData.detail.accepted.includes("functional")) {
+//       removePlaceholder();
+//     }
+//     else if (!checkPlaceholder()){
+//        addPlaceholder();
+//     }
+// });
 
-  function removePlaceholder() {
-    const embedResponsiveElement = document.querySelector(
-      ".helloMuhammed"
-    );
-    if (embedResponsiveElement) {
-      const divElementToRemove = embedResponsiveElement.querySelector(
-        ".video-placeholder-youtube"
-      );
-      if (divElementToRemove) {
-        embedResponsiveElement.removeChild(divElementToRemove);
-      }
-    }
-  }
-  function checkPlaceholder() {
-    return document.querySelector(".video-placeholder-youtube");
-  }
+
+// function custumFunction() {
+//   const consent = getCkyConsent();
+//   if (!consent.categories.functional && !checkPlaceholder()) {
+//     addPlaceholder();
+//   } else if (consent.categories.functional) {
+//     removePlaceholder();
+//   }
+//  }
+
+//   function addPlaceholder() {
+//     const divElement = document.createElement("div");
+//     divElement.setAttribute("class", "video-placeholder-youtube");
+//     divElement.setAttribute("data-cky-tag", "video-placeholder");
+//     divElement.setAttribute(
+//       "style",
+//       "height: 100%; width: 100%; background-image: url(https://cdn-cookieyes.com/assets/images/cky-placeholder.svg); background-size: 80px; background-position: center; background-repeat: no-repeat; background-color: #b2b0b059; display: flex; align-items: flex-end;"
+//     );
+//     const paragraph = document.createElement("p");
+//     paragraph.setAttribute("class", "video-placeholder-text-youtube");
+//     paragraph.setAttribute("data-cky-tag", "placeholder-title");
+//     paragraph.setAttribute(
+//       "style",
+//       "display: block; border-color: rgb(0, 0, 0); background-color: rgb(0, 0, 0); color: rgb(255, 255, 255);"
+//     );
+//     paragraph.textContent = "Please accept cookies to access this content";
+//     divElement.appendChild(paragraph);
+//     //pointing to your vedio container
+//     const embedResponsiveElement = document.querySelector(
+//       "body > div.h-full.relative.grainy > main > div.flex.flex-col.lg\\:flex-row-reverse.bg-\\[\\#f9fafb\\].text-black.justify-center > div:nth-child(2) > div > div.flex.flex-col.lg\\:flex-col.items-center.shadow-lg.rounded-md.p-8.border-custom > div.flex-\\[2\\].mt-16.lg\\:mt-0.lg\\:mr-8 > div.mt-4.video-frame.framed.h-full.w-full > div:nth-child(1)"
+//     );
+//     if (embedResponsiveElement) {
+//       embedResponsiveElement.appendChild(divElement);
+//     }
+//   }
+
+//   function removePlaceholder() {
+//     //pointing to your vedio container
+//     const embedResponsiveElement = document.querySelector(
+//       "body > div.h-full.relative.grainy > main > div.flex.flex-col.lg\\:flex-row-reverse.bg-\\[\\#f9fafb\\].text-black.justify-center > div:nth-child(2) > div > div.flex.flex-col.lg\\:flex-col.items-center.shadow-lg.rounded-md.p-8.border-custom > div.flex-\\[2\\].mt-16.lg\\:mt-0.lg\\:mr-8 > div.mt-4.video-frame.framed.h-full.w-full > div:nth-child(1)"
+//     );
+//     if (embedResponsiveElement) {
+//       const divElementToRemove = embedResponsiveElement.querySelector(
+//         ".video-placeholder-youtube"
+//       );
+//       if (divElementToRemove) {
+//         embedResponsiveElement.removeChild(divElementToRemove);
+//       }
+//     }
+//   }
+//   function checkPlaceholder() {
+//     return document.querySelector(".video-placeholder-youtube");
+//   }
